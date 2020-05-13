36121d671b5aada8d88ae7977201f120b8f32bd5
Derek Yang <derek.yang7@gmail.com>
2020-05-13 15:45:29 -0400

    first commit

---
+++ first-app/src/components/Test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Component } from "react";
+import { Button, Card } from 'react-bootstrap'
+class Test extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <p>My first app!</p>
+                <p>
+                    <Button variant="warning">
+                        My Button
+                    </Button>
+                </p>
+
+                <Card >
+                    <Card.Header as="h5" class='body-text'>Hello Derek</Card.Header>
+                    <Card.Body>
+
+                        <Card.Text class='body-text'>My name is Derek</Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+        );
+    }
+}
+
+export default Test;
+
+
